0ed0cb58f4f40c8c0aae90146e82f080784ba86e
virag-jain-qed <virag.jain@qed42.com>
2025-02-12 11:40:54 +0530

    Added export config

---
+++ web/sites/default/files/php/twig/67a487bd1fecc_filter-caption.html.twig_yBdz2Rpu2udHGJapMcySIBSx6/VG0NgsdNmru5KlsnS76h6FBpnBStLMwvMnaEJTkcfBc.php
@@ -0,0 +1,122 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* core/themes/claro/templates/classy/content-edit/filter-caption.html.twig */
+class __TwigTemplate_2c6c2598f38399de3582a8c480828e8d extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 15
+        yield "<figure role=\"group\" class=\"caption caption-";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["tag"] ?? null), "html", null, true);
+        if (($context["classes"] ?? null)) {
+            yield " ";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["classes"] ?? null), "html", null, true);
+        }
+        yield "\">
+";
+        // line 16
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["node"] ?? null), "html", null, true);
+        yield "
+<figcaption>";
+        // line 17
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["caption"] ?? null), "html", null, true);
+        yield "</figcaption>
+</figure>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["tag", "classes", "node", "caption"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "core/themes/claro/templates/classy/content-edit/filter-caption.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  57 => 17,  53 => 16,  44 => 15,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "core/themes/claro/templates/classy/content-edit/filter-caption.html.twig", "/var/www/html/web/core/themes/claro/templates/classy/content-edit/filter-caption.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["if" => 15];
+        static $filters = ["escape" => 15];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
